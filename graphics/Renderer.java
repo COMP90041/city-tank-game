3deb9ca1404da13bb4a77c599d29135ddd9a3da3
Thuan Pham <thuan.pham@unimelb.edu.au>
2020-09-10 14:57:59 +1000

    Initial commit

---
+++ graphics/Renderer.java
@@ -0,0 +1,87 @@
+package graphics;
+
+import objects.*;
+
+public class Renderer
+{
+	private Tank[] tanks;
+	private int tankCount;
+	private int width, height;
+
+	private char borderChar = '.';
+
+	public Renderer(int aWidth, int aHeight)
+  {
+		width = aWidth;
+		height = aHeight;
+		tanks = new Tank[21];
+		tankCount = 0; 
+	}	
+
+	public void addTank(Tank aTank)
+	{
+		if (tankCount < 21)
+		{
+			tanks[tankCount] = aTank;
+			tankCount++;
+		}
+		else
+		{
+			System.out.println("Fatal Error!");
+			System.exit(1);
+		}
+	}
+
+	public void render()
+	{
+		int i, j;
+		char[][] bitmap = new char[height][width];
+		//clear the screen
+		for (i = 0; i < height; i++)
+		{
+			for (j = 0; j < width; j++)
+			{
+				bitmap[i][j] = ' ';
+			}
+		}
+		//render objects
+		for (int index = 0; index < tankCount; index++)
+		{
+			Tank aTank = tanks[index];
+			char[][] aTankBitmap = aTank.draw();
+			for (i = 0; i < aTank.getHeight(); i++)
+			{
+				for (j = 0; j < aTank.getWidth(); j++)
+				{
+					bitmap[aTank.getRowPos() + i][aTank.getColPos() + j] = aTankBitmap[i][j];
+				}
+			}
+		}
+	  //output to the screen
+    //including the border
+		for (i = 0; i < width + 2; i++)
+		{
+			System.out.print(Color.ANSI_RED + borderChar + Color.ANSI_RESET);
+		}
+		System.out.println();
+
+		//main playing screen
+		for (i = 0; i < height; i++)
+    {
+			System.out.print(Color.ANSI_RED + borderChar + Color.ANSI_RESET);
+      for (j = 0; j < width; j++)
+      {
+        System.out.print(bitmap[i][j]);
+      }
+			System.out.println(Color.ANSI_RED + borderChar + Color.ANSI_RESET);
+    }
+
+		for (i = 0; i < width + 2; i++)
+		{
+			System.out.print(Color.ANSI_RED + borderChar + Color.ANSI_RESET);
+		}
+		System.out.println();
+
+	}
+}
+
---
+++ utilities/Commands.java
@@ -0,0 +1,12 @@
+package utilities;
+
+public class Commands
+{
+  public enum Direction {UP, DOWN, LEFT, RIGHT}
+ 
+	public static void clearScreen()
+	{
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+}
